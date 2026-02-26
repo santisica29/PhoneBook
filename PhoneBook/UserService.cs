95646e0e98a183f16c32b3f0b988761861d6fbf5
Santiago Sica <santisica29@gmail.com>
2026-02-26 12:38:24 -0300

    Create Service class to separate the main menu from the controller

--- PhoneBook/UserService.cs
+++ PhoneBook/UserService.cs
@@ -1,5 +1,27 @@
-﻿namespace PhoneBook;
+﻿using PhoneBook.Controllers;
+using PhoneBook.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook;
 
 internal class UserService
 {
+    private UserController userController = new UserController(); 
+    internal void AddUser()
+    {
+        var name = Validator.GetStringInput();
+        var email = Validator.GetEmailInput();
+        var phoneNumber = Validator.GetPhoneNumber();
+        var address = Validator.GetStringInput();
+
+        var newUser = new User() 
+        {
+            Name = name,
+            Email = email,
+            PhoneNumber = phoneNumber,
+            Address = address,
+        };
+
+        userController.Add(newUser);
+    }
 }
